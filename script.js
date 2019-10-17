71e155592b859c2fdfd49bdf775cd888f3d88a71
Miked0331 <40614967+Miked0331@users.noreply.github.com>
2019-10-17 15:48:34 -0400

    all c4 output working

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
 const c4 = 1;
 
-const output = {
-    metal: + c4 * 200,
-    tech: c4 * 2,
-    sulf: c4 * 2200,
-    cloth: c4 * 20,
-    animal_fat: c4 * 45,
-    charcoal: c4 * 3000,
-    explosives: c4 * 20
-}
-
+const output = [
+    { metal: + c4 * 200 },
+    { tech: c4 * 2 },
+    { sulf: c4 * 2200 },
+    { cloth: c4 * 20 },
+    { animal_fat: c4 * 45 },
+    { charcoal: c4 * 3000 },
+    { explosives: c4 * 20 }
+]
 
 
 function myFunction() {
     var input = prompt("Please enter a number", "Enter a Number");
     if (input != null) {
-        document.getElementById("demo").innerHTML =
-            "You need " + output.metal * input + ' metal frags <br>' +
-            "You need " + output.tech * input + ' tech trash <br>';
+        document.getElementById("c4").innerHTML =
+            "You need " + output[0].metal * input + ' metal frags <br>' +
+            "You need " + output[1].tech * input + ' tech trash <br>' +
+            "You need " + output[2].sulf * input + ' sulfur <br>' +
+            "You need " + output[3].cloth * input + ' cloth <br>' +
+            "You need " + output[4].animal_fat * input + ' animal fat <br>' +
+            "You need " + output[5].charcoal * input + ' charcoal <br>' +
+            "You need " + output[6].explosives * input + ' explosives <br>'
     }
 }
 
