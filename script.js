635a291ff7e5285f19a636d2858b9836accdcdb0
Miked0331 <40614967+Miked0331@users.noreply.github.com>
2019-10-17 22:24:30 -0400

    test

--- script.js
+++ script.js
@@ -14,13 +14,16 @@ function myFunction() {
     var input = prompt("Please enter a number", "Enter a Number");
     if (input != null) {
         document.getElementById("c4").innerHTML =
-            "You need " + output[0].metal * input + ' metal frags <br>' + output[0].img +
+            "You need " + output[0].metal * input + ' metal frags <br>' +
             "You need " + output[1].tech * input + ' tech trash <br>' +
             "You need " + output[2].sulf * input + ' sulfur <br>' +
             "You need " + output[3].cloth * input + ' cloth <br>' +
             "You need " + output[4].animal_fat * input + ' animal fat <br>' +
             "You need " + output[5].charcoal * input + ' charcoal <br>' +
             "You need " + output[6].explosives * input + ' explosives <br>'
+    }
+    else {
+        alert("You have not entered a number!, try again.")
     }
 }
 
@@ -29,5 +32,17 @@ const rockets = 1;
 const output2 = [
     { metal: rockets * 100 },
     { pipes: rockets * 2 },
-    {}
+    { sulfur: rockets * 1400 },
+    { charcoal: rockets * 1950 },
+    { cloth: rockets * 8 },
+    { animal_fat: rockets * 24 }
 ]
+
+const myFunction2 = () => {
+    const input = prompt("How many rockets do you need to craft?", "Enter Number");
+    if (input != null) {
+        document.getElementById("rockets").innerHTML =
+            "You need <br>" +
+            output2[0].metal
+    }
+}
